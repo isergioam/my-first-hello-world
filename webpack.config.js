7d7b69d741ec8782aed9ddda995e5184208c878d
isergioam <isergioam@gmail.com>
2018-12-10 23:22:35 +0100

    Versión definitiva.

--- webpack.config.js
+++ webpack.config.js
@@ -1,99 +1,81 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpack = require('webpack');
-
-var path = require('path');
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let webpack = require('webpack');
 
 var basePath = __dirname;
 
 module.exports = {
-    context: path.join(basePath, 'src'),
-    resolve: {
-        extensions: ['.js', '.ts']
+ context: path.join(basePath, 'src'),
+ resolve: {
+   extensions: ['.js', '.ts', '.tsx']
+ },
+ entry: {
+   app: './index.ts',
+   appStyles: './content/scss/styles.scss',
+   vendorStyles: [
+     '../node_modules/bootstrap/dist/css/bootstrap.css',
+   ],
+ },
+ output: {
+   path: path.join(basePath, 'dist'),
+   filename: '[name].js',
+ },
+ module: {
+   rules: [
+     {
+       test: /\.tsx?$/,
+       exclude: /node_modules/,
+       loader: 'awesome-typescript-loader',
+       options: {
+         useBabel: true,
+       },
+     },
+     {
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader",
+      ]
     },
-    entry: {
-        app: './content/ts/students.ts',
-        appStyles: [
-        './content/scss/mystyles.scss',
-        ],
-        vendor: [
-        'jquery',
-        ],
-        vendorStyles: [
-            '../node_modules/bootstrap/dist/css/bootstrap.css',
-        ],
+     {
+       test: /\.css$/,
+       use: [MiniCssExtractPlugin.loader, "css-loader"],
+     },
+     {
+       test: /\.(png|jpg|gif|svg)$/,
+       exclude: /node_modules/,
+       use: {
+      loader: 'url-loader',
+      options: {
+      limit:5000,
+        name: './img/[hash].[name].[ext]'
+     },
     },
-    output: {
-        filename: '[chunkhash].[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                loader: 'awesome-typescript-loader',
-                options: {
-                    useBabel: true,
-                },
-            },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: {
-                    loader: 'css-loader',
-                },
-                use: [
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                    },
-                ],
-            }),
-        },
-        {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: {
-                    loader: 'css-loader',
-                },
-            }),
-        },
-        {
-            test: /\.(png|jpg)$/,
-            exclude: /node_modules/,
-            loader: 'url-loader?limit=5000',
-        },
-        {
-            test: /\.html$/,
-            loader: 'html-loader',
-        },   
-        ],
-    },
-    plugins: [
-        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
-        new HtmlWebpackPlugin({
-            filename: 'index.html', //Name of file in ./dist/
-            template: 'index.html', //Name of template in ./src
-            hash: true,
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-        new ExtractTextPlugin({
-            filename: '[chunkhash].[name].css',
-            disable: false,
-            allChunks: true,
-        }),
-    ],
+  },
+     {
+       test: /\.html$/,
+       loader: 'html-loader',
+      },   
+   ],
+ },
+ // For development https://webpack.js.org/configuration/devtool/#for-development
+ devtool: 'inline-source-map',
+ devServer: {
+   port: 8080,
+   noInfo: true,
+ },
+ plugins: [
+   //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+   new HtmlWebpackPlugin({
+     filename: 'index.html', //Name of file in ./dist/
+     template: 'index.html', //Name of template in ./src
+     hash: true,
+   }),
+   new MiniCssExtractPlugin({
+     filename: "[name].css",
+     chunkFilename: "[id].css"
+   }),
+ ],
 };
